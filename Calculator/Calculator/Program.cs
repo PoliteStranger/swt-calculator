9e19e99340615400859f075c51667618b85c9b7b
PoliteStranger <oliver09@spange.com>
2022-02-09 17:45:23 +0100

    added file

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿//See httusing System;
+
+namespace Calculator // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc();
+            Console.WriteLine($"Test add {calc.Add(2, 4)}");
+            Console.WriteLine($"Test pow {calc.Power(2, 3)}");
+
+            //ineffecient testing, not reusable!
+            //not scalable!!
+        }
+    }
+}
